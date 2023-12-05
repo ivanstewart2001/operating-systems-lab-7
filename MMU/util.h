8a31934a7fa53c907b3ad51dfda3574e1d3e706f
Ivan Stewart <ivan.stewart@bison.howard.edu>
2023-12-05 03:24:57 +0000

    lab 7

---
+++ MMU/util.h
@@ -0,0 +1,13 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+// Collaborated with Lauren Guthrie
+
+/**
+ * Utility function file
+ */
+
+
+void parse_file(FILE *, int [][2], int *, int *);
+
+#endif				// UTIL_H
